bf5f89337b1c497303f998ea0be3a4314612da5d
harshitpratapsingh01 <harshit.singh@appinventiv.com>
2023-09-25 10:48:57 +0530

    changes

--- src/utils/DbConnection.ts
+++ src/utils/DbConnection.ts
@@ -1,15 +1,18 @@
-import { Sequelize } from "sequelize";
-import * as dotenv from 'dotenv'
+const Sequelize = require('sequelize');
+import dotenv from 'dotenv'
 dotenv.config();
 
-const sequelize = new Sequelize({
-    dialect: 'postgres',
-    host: 'localhost',
-    port: 5432,
-    username: 'postgres',
-    password: 'harshit@123',
-    database: 'Mydb',
-});
+const sequelize = new Sequelize(process.env.DB_NAME,process.env.DB_USER,process.env.DB_PASS,{
+    host:process.env.DB_HOST,
+    dialect:process.env.DB_DIALECT,
+    pool:{
+        max:10,
+        min:0,
+        aquire:3000,
+        idle:10000
+    }
+  });
+
 
 
 class Connection {
